4316a7be3a0d576dae61a258853fe03623d571dd
Charmingloopy <111189434+Charmingloopy@users.noreply.github.com>
2024-06-21 20:54:27 +0100

    change magnitude effect

--- Data/Scripts/011_Battle/002_Move/006_Move_Effects_080-0FF.rb
+++ Data/Scripts/011_Battle/002_Move/006_Move_Effects_080-0FF.rb
@@ -395,37 +395,49 @@ end
 
 #===============================================================================
 # Power is chosen at random. Power is doubled if the target is using Dig. Hits
-# some semi-invulnerable targets. (Magnitude)
+# some semi-invulnerable targets. If rework is enabled size 8+ magnitudes set up stealth rock (Magnitude)
 #===============================================================================
+
 class PokeBattle_Move_095 < PokeBattle_Move
-  def hitsDiggingTargets?; return true; end
-
-  def pbOnStartUse(user,targets)
-    baseDmg = [10,30,50,70,90,110,150]
-    magnitudes = [
-       4,
-       5,5,
-       6,6,6,6,
-       7,7,7,7,7,7,
-       8,8,8,8,
-       9,9,
-       10
-    ]
-    magni = magnitudes[@battle.pbRandom(magnitudes.length)]
-    @magnitudeDmg = baseDmg[magni-4]
-    @battle.pbDisplay(_INTL("Magnitude {1}!",magni))
+    def hitsDiggingTargets?; return true; end
+  
+    def pbOnStartUse(user,targets)
+      baseDmg = [10,30,50,70,90,110,150]
+      magnitudes = [
+         4,
+         5,5,
+         6,6,6,6,
+         7,7,7,7,7,7,
+         8,8,8,8,
+         9,9,
+         10
+      ]
+      magni = magnitudes[@battle.pbRandom(magnitudes.length)]
+      @magnitudeDmg = baseDmg[magni-4]
+      @battle.pbDisplay(_INTL("Magnitude {1}!",magni))
+      @magnisize = magni
+    end
+    
+    def pbEffectAgainstTarget(user, target)
+     if @magnisize >= 8 then
+        if $PokemonSystem.magnitude_rework == 1 then
+         target.pbOwnSide.effects[PBEffects::StealthRock] = true
+         @battle.pbDisplay(_INTL("The size {1} magnitude caused stealth rocks to land on to the field!",@magnisize))
+        end
+      end
+     end
+ 
+    def pbBaseDamage(baseDmg,user,target)
+      return @magnitudeDmg
+    end
+  
+    def pbModifyDamage(damageMult,user,target)
+      damageMult *= 2 if target.inTwoTurnAttack?("0CA")   # Dig
+      damageMult /= 2 if @battle.field.terrain == :Grassy
+      return damageMult
+    end
   end
 
-  def pbBaseDamage(baseDmg,user,target)
-    return @magnitudeDmg
-  end
-
-  def pbModifyDamage(damageMult,user,target)
-    damageMult *= 2 if target.inTwoTurnAttack?("0CA")   # Dig
-    damageMult /= 2 if @battle.field.terrain == :Grassy
-    return damageMult
-  end
-end
 
 
 
